8797f76ac0ebc203b41ea55a104d8a3c9d36e1d6
Joey Kaan <joey94jk@gmail.com>
2015-03-09 15:39:41 +0100

    - Remove unneccesary files because of Mackup & add and modify ones to my needs

--- brew.sh
+++ brew.sh
@@ -21,10 +21,13 @@ sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
+
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
+
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
+
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
@@ -50,10 +53,25 @@ brew install woff2
 # Install other useful modules
 brew install git
 brew install speedtest_cli
+brew install zopfli
+brew install pigz
+
 
 # Install Node.js. Note: this installs `npm` too, using the recommended
 # installation method.
 brew install node
+
+# Install applications through Brew cask
+brew cask install dash
+brew cask install spectacle
+brew cask install flux
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install spotify
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install vlc
 
 # Remove outdated versions from the cellar.
 brew cleanup
--- install-deps.sh
+++ install-deps.sh
@@ -1,3 +1,12 @@
 # Install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Create folder which is used to store applications
+mkdir -p ~/Development/Applications
+
+# Clone the rupa/z application
+git clone git@github.com:rupa/z.git ~/Development/Applications/z
+
+# Install GMVault
+pip install gmvault
