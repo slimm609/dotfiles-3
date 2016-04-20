c29a7ddd3573320cf01dce2c0c4ca1170e55ca12
Joey Kaan <joey94jk@gmail.com>
2016-04-20 11:22:35 +0200

    When RVM is installed, ruby is now also installed by default

--- install-deps.sh
+++ install-deps.sh
@@ -12,7 +12,7 @@ cp init/hub.zsh_completion ~/.zsh/completions/ \
 
 # Install RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
-  curl -sSL https://get.rvm.io | bash -s stable
+  curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # Install trash, a safer alternative to 'rm'
 npm install -g trash
