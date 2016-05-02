efae57902af1ee0fce7c1183ea03d3e1c18ec80e
Joey Kaan <joey94jk@gmail.com>
2016-05-02 10:03:08 +0200

    Add useful git extras, alias for docker command & extend path
    Added alias for a very long docker command which is very hard to
    remember.

--- brew.sh
+++ brew.sh
@@ -105,6 +105,7 @@ brew install webkit2png
 brew install zopfli
 brew install reattach-to-user-namespace
 brew install gpg
+brew install git-extras
 
 # Install Node.js and fix a common problem with Node's brew: https://gist.github.com/DanHerbert/9520689
 brew install node --without-npm
