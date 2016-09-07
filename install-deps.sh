d823a2eadd433eb5f9adb9f492e11da3294235d9
Joey Kaan <joey94jk@gmail.com>
2016-09-07 21:48:30 +0200

    Simplify

--- install-deps.sh
+++ install-deps.sh
@@ -6,13 +6,12 @@ git clone git@github.com:rupa/z.git ~/Development/Applications/z
 
 # Add Hub's completion to zsh
 mkdir -p ~/.zsh/completions
-
-cp init/hub.zsh_completion ~/.zsh/completions/ \
-      mv ~/.zsh/completions/hub.zsh_completion ~/.zsh/completions/_hub
+cp init/hub.zsh_completion ~/.zsh/completions/
+mv ~/.zsh/completions/hub.zsh_completion ~/.zsh/completions/_hub
 
 # Install RVM
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
-  curl -sSL https://get.rvm.io | bash -s stable --ruby
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # Install trash, a safer alternative to 'rm'
 npm install -g trash-cli
