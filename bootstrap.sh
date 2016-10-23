a7b0a1baa11ec053156bc3f9ae8cdc7214ba5362
Joey Kaan <j.kaan@coolblue.nl>
2016-10-23 17:32:03 +0200

    Remove hub completion install

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,21 +2,9 @@
 
 git pull origin master;
 
-git submodule foreach git pull origin master;
-
-# Create zsh_completions folder and add the hub.zsh_completions to it
-mkdir -p $HOME/.zsh_completions
-curl https://github.com/github/hub/blob/master/etc/hub.zsh_completion >  ~/.zsh_completions/_hub;
-
-# Download imgcat folder, give it the right permissions and put it in the correct folder
-wget -q --show-progress https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat;
-chmod +x imgcat;
-mkdir -p ${HOME}/bin && cp imgcat ${HOME}/bin/imgcat;
-rm -rf imgcat;
-
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude ".gitmodules" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "utils/" --exclude "install-prezto.zsh" --exclude "install-deps.sh" --exclude "brew.sh"  -avh --no-perms . ~;
+		--exclude "README.md" --exclude "macos.sh" --exclude "LICENSE-MIT.txt" --exclude "bin/" --exclude "utils/" --exclude "install-zim.zsh" --exclude "install-deps.sh" --exclude "brew.sh"  -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
--- install-deps.sh
+++ install-deps.sh
@@ -4,11 +4,6 @@ mkdir -p ~/Development/Applications
 # Clone the rupa/z application
 git clone git@github.com:rupa/z.git ~/Development/Applications/z
 
-# Add Hub's completion to zsh
-mkdir -p ~/.zsh/completions
-cp init/hub.zsh_completion ~/.zsh/completions/
-mv ~/.zsh/completions/hub.zsh_completion ~/.zsh/completions/_hub
-
 # Install RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
@@ -16,5 +11,5 @@ curl -sSL https://get.rvm.io | bash -s stable --ruby
 # Install trash, a safer alternative to 'rm'
 npm install -g trash-cli
 
-# Install prezto, my framework of choice for ZSH
-./install-prezto.zsh
+# Install ZSH config framework
+./install-zim.zsh
