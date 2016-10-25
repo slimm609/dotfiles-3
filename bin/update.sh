ba5431d6fea70030483c12eeabede55c8df092e0
Joey Kaan <j.kaan@coolblue.nl>
2016-10-25 19:55:30 +0200

    Update the update command to exclude php stuff and update zim

--- bin/update.sh
+++ bin/update.sh
@@ -1,9 +1,6 @@
 sudo softwareupdate -i -a
 brew update
-brew unlink php56
-brew unlink php70
 brew upgrade
-brew link php70
 brew cleanup
 npm install npm -g
 npm update -g
@@ -12,3 +9,4 @@ gem update
 
 Z_DIR=~/Development/Applications/z
 git --git-dir=${Z_DIR}/.git --work-tree=$Z_DIR pull
+zmanage update
