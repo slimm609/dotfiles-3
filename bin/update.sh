a86265dd3f29e685042bbb19703f31e875121266
Joey Kaan <j.kaan@coolblue.nl>
2016-10-30 10:48:16 +0100

    Add remote and branch to git pull commands
    Since Zim is a submodule it's just a reference to a commit so to
    actually get the latest changes from the remote 'origin master' should
    be added.

--- bin/update.sh
+++ bin/update.sh
@@ -10,9 +10,9 @@ gem update
 
 Z_DIR=~/Development/Applications/z
 echo "Updating Z"
-git --git-dir=${Z_DIR}/.git --work-tree=$Z_DIR pull
+git --git-dir=${Z_DIR}/.git --work-tree=$Z_DIR pull origin master
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ZIM_DIR=${SCRIPT_DIR}/../home/.zsh/.zim
 echo "Updating Zim"
-git --git-dir=${ZIM_DIR}/.git --work-tree=$ZIM_DIR pull
+git --git-dir=${ZIM_DIR}/.git --work-tree=$ZIM_DIR pull origin master
