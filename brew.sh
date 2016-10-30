704dd9fd88a7e90eac0a343bf311f0e670577033
Joey Kaan <j.kaan@coolblue.nl>
2016-10-30 14:20:46 +0100

    Remove all not-used kegs from brew

--- brew.sh
+++ brew.sh
@@ -19,21 +19,11 @@ brew upgrade --all
 brew install coreutils
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
-
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
 
 # Install ZSH
 brew install zsh
@@ -43,66 +33,11 @@ brew install wget --with-iri
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-brew install homebrew/php/php56 --with-gmp
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
-# Install other useful binaries.
-brew install ack
-brew install dark-mode
-
-#brew install exiv2
 brew install git
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zi
-brew install pigz
-brew install gzip
-brew install speedtest_cli
 brew install ack
 brew install hub
-brew install ssh-copy-id
 brew install mackup
-brew install unar
-brew install ssh-copy-id
-brew install testssl
-brew install tree
-brew install webkit2png
-brew install zopfli
 brew install reattach-to-user-namespace
 brew install gpg
 brew install git-extras
