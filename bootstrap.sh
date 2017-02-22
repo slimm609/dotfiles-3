550c36810666cd143de91c06b94b877299fe8257
Joey Kaan <joey94jk@gmail.com>
2017-02-22 18:26:32 +0100

    Remove php 5.6 (living on the edge babee)

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 elif [ "$1" == "--test" ]; then
   mkdir ~/dotfiles-test
-  rsync . ~/dotfiles-test
+  rsync -avh . ~/dotfiles-test
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
