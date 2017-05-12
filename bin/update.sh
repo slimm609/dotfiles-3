f11cec123cf8b6213b24e7b6784c061c12da8af0
Joey Kaan <jkaan@users.noreply.github.com>
2017-05-12 12:07:04 +0200

    Add python requirements file and updating to update script

--- bin/update.sh
+++ bin/update.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 sudo softwareupdate -i -a
+
 brew update
 brew bundle
+
 npm install npm -g
 npm update -g
+
 gem update --system
 gem update
 rvm get stable --ignore-dotfiles
+
+# Update requirements
+# -z option will return exit code 11 when packages have updates
+pur -z -r requirements.txt
+if [ $? -eq 11 ]; then
+  pip install -r requirements.txt
+fi
 
 Z_DIR=~/Development/Applications/z
 echo "Updating Z"
