07ae2fd17702344c9dc3efb842b2a3103ec5b8ad
Joey Kaan <jkaan@users.noreply.github.com>
2017-10-18 20:39:42 +0200

    Remove install of trash-cli from install-deps script
    No longer need this because this is already installed by the Npmfile

--- install/install-deps.sh
+++ install/install-deps.sh
@@ -20,6 +20,3 @@ cd $CURRENT_DIR
 # Install RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
-
-# Install trash, a safer alternative to 'rm'
-npm install -g trash-cli
