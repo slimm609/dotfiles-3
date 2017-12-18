604b7b7de79fcd19652245120379802aeff5fdc3
Joey Kaan <jkaan@users.noreply.github.com>
2017-12-18 19:49:15 +0100

    Add install of Homestead to dotfiles

--- install/install-deps.sh
+++ install/install-deps.sh
@@ -20,3 +20,6 @@ cd $CURRENT_DIR
 # Install RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+# Install Homestead, the best PHP Vagrant box
+git clone git@github.com:laravel/homestead.git ~/Development/Applications/Homestead
