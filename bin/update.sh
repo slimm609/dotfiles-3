da404a3f819b84a5569a0c55300b1aeacd88bc17
Joey Kaan <jkaan@users.noreply.github.com>
2018-03-28 20:46:14 +0200

    Get rid of requirements.txt
    I am really no longer using Python so no need

--- bin/update.sh
+++ bin/update.sh
@@ -9,13 +9,6 @@ cat Npmfile | xargs npm i -g
 # Update RVM
 rvm get stable --ignore-dotfiles
 
-# Update requirements
-# -z option will return exit code 11 when packages have updates
-pur -z -r requirements.txt
-if [ $? -eq 11 ]; then
-  pip install -r requirements.txt
-fi
-
 Z_DIR=~/Development/Applications/z
 echo "Updating Z"
 git --git-dir=${Z_DIR}/.git --work-tree=$Z_DIR pull origin master
