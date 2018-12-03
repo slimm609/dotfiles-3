e8d4c3410c9bf6760def5a6bbd19957bb3c18c0b
Joey Kaan <jkaan@users.noreply.github.com>
2018-12-03 19:13:09 +0100

    Remove homestead since using docker now

--- install/install-deps.sh
+++ install/install-deps.sh
@@ -8,8 +8,5 @@ git clone git@github.com:rupa/z.git ~/Development/Applications/z
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 
-# Install Homestead, the best PHP Vagrant box
-git clone git@github.com:laravel/homestead.git ~/Development/Applications/Homestead
-
 # Setup bat
 bat cache --init --source ~/.dotfiles/bat
