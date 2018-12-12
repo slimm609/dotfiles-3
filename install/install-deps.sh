6c0291c67525e42821e672ec7f079734c3f9f263
Joey Kaan <jkaan@users.noreply.github.com>
2018-12-12 21:00:56 +0100

    Ignore dotfiles when installing RVM since RVM is loaded in one dotfile already

--- install/install-deps.sh
+++ install/install-deps.sh
@@ -9,7 +9,7 @@ git clone git@github.com:rupa/z.git ~/Development/Applications/z
 
 # Install RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable --ruby
+curl -sSL https://get.rvm.io | bash -s stable --ruby --ignore-dotfiles
 
 # Setup bat
 bat cache --init --source ~/.dotfiles/bat
