6024d3b012bdb9d9dde44621ab2418da37aa40be
Joey Kaan <jkaan@users.noreply.github.com>
2019-10-08 14:21:13 +0200

    Fix script

--- login-calendar/add_event.sh
+++ login-calendar/add_event.sh
@@ -9,7 +9,7 @@ NEXT_DAY=$(date -v "+8H" -v "+30M" +"%D")
 TIME=$(date -v "+8H" -v "+30M" +"%T")
 if ! (gcalcli agenda | grep -q 'Naar huis'); then
   if [[ $(date +%u) -lt 6 ]]; then
-    if [ "$CURRENT_DAY" == "$NEXT_DAY" ]; then
+    if [ "$CURRENT_DAY" = "$NEXT_DAY" ]; then
       if [[ "$TIME" > "14:00:00" && "$TIME" < "20:00:00" ]]; then
         /usr/bin/env gcalcli add --title "Naar huis" --when "$DATE_TO_ADD_EVENT" --duration 0 --calendar "Main Calendar" --noprompt
       fi
